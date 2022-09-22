ec05a1f00cb1061d7ecc5aa9130c55605259a37b
AdamGotman <70971376+AdamGotman@users.noreply.github.com>
2022-09-22 08:56:54 +0300

    Update server.lua

--- adm_Comenzi/server.lua
+++ adm_Comenzi/server.lua
@@ -9,8 +9,7 @@ RegisterCommand('areviveall',function(source)
 	local name = vRP.getPlayerName({source})
 	local staff = vRP.isUserCoFondator({user_id})
 	if not staff then return TriggerClientEvent('chatMessage',source,'^1Error:^0 Nu deti un grad staff') end 
-	local revall = vRPclient.varyHealth(-1,{100}) TriggerClientEvent('chatMessage',-1,'Admin-ul'..name..'a dat revive la tot server-ul.')
-	if revall then return  end
+        vRPclient.varyHealth(-1,{100}) TriggerClientEvent('chatMessage',-1,'Admin-ul'..name..'a dat revive la tot server-ul.')
   end)
   RegisterCommand('respawn',function(source,args)
 	local user_id = vRP.getUserId({source})
@@ -21,8 +20,8 @@ RegisterCommand('areviveall',function(source)
 	local target_nil = target_id == nil
 	if not staff then return TriggerClientEvent('chatMessage',source,'^1Error: ^0Nu deti un grad de staff') end
 	if target_nil then return TriggerClientEvent('chatMessage',source,'^1Error:^0 Selecteaza un id /respawn') end
-	local respawn = vRPclient.teleport(target_id,{-779.55718994141,339.3352355957,160.00148010254}) TriggerClientEvent('chatMessage',-1,'Admin-ul ^1'..GetPlayerName(source)..'['..user_id..'] ^0i-a dat respawn lui ^1'..GetPlayerName(user_id_target)..'['..user_id_target..'] ^0') vRPclient.varyHealth(target,{100})   vRPclient.notify(target,{'Ai primit respawn de la admin-ul '..GetPlayerName(user_id_target)})
-	if respawn then return  end;
+	 respawn = vRPclient.teleport(target_id,{-779.55718994141,339.3352355957,160.00148010254}) TriggerClientEvent('chatMessage',-1,'Admin-ul ^1'..GetPlayerName(source)..'['..user_id..'] ^0i-a dat respawn lui ^1'..GetPlayerName(user_id_target)..'['..user_id_target..'] ^0') vRPclient.varyHealth(target,{100})   vRPclient.notify(target,{'Ai primit respawn de la admin-ul '..GetPlayerName(user_id_target)})
+	
   end)
 RegisterCommand('arevive',function(source,args)
 	local user_id = vRP.getUserId({source})
@@ -32,13 +31,13 @@ RegisterCommand('arevive',function(source,args)
 	local argszero = args[1] == nil
     if not staff then return TriggerClientEvent('chatMessage',source,'^1Error:^0 Nu deti un grad staff') end
     if argszero then return TriggerClientEvent('chatMessage',source,'^1Error:^0 Selecteaza un id /arevive id') end
-	local arevive = vRPclient.varyHealth(target_id,{100}) TriggerClientEvent('chatMessage',-1,'Admin-ul^1 ' ..GetPlayerName(source)..'^0[^1'..user_id..'^0] a dat revive lui ^1'  ..GetPlayerName(target_id)..'^0[^1'..user_id..'^0]')
-	if arevive then return end
+	 arevive = vRPclient.varyHealth(target_id,{100}) TriggerClientEvent('chatMessage',-1,'Admin-ul^1 ' ..GetPlayerName(source)..'^0[^1'..user_id..'^0] a dat revive lui ^1'  ..GetPlayerName(target_id)..'^0[^1'..user_id..'^0]')
+	
 end)
 RegisterCommand('noclip',function(source)
 	local user_id = vRP.getUserId({source})
 	local staff = vRP.isUserAdmin({user_id})
 	if not staff then return TriggerClientEvent('chatMessage',source,'^1Error:^0 Nu deti un grad staff') end
-	local nc = vRPclient.toggleNoclip(source, {})
-	if nc then return end
+	 nc = vRPclient.toggleNoclip(source, {})
+	
 end)
